212eb808aac9315078c6e34818adb1eeedc46136
JustynaZienk <justyna.buchnowska@gmail.com>
2026-01-03 12:40:10 +0000

    Dodanie klasy dziecko

--- klasy.py
+++ klasy.py
@@ -1,18 +1,46 @@
-#Klasa
+#Klasa = Szablon, Przepis
 class Czlowiek:
     gatunek = "Homo Sapiens"
-    def __init__(self, imie):
+    def __init__(self, imie, plec):
         # Konstruktor
         #Akt Istnienia
-        print(f"Niech powstanie czowiek o imieniu {imie}")
+        print(f"Niech powstanie czlowiek o imieniu {imie}")
         self.imie =imie
-        #ada.imie = "Adam"
-#Powstawanie obiektu
+        self.plec= plec
+        #adam.imie = "Adam"
+
+        #Metoda
+    def przedstaw_sie(self):
+        print(f"dzien dobry mam na imie {self.imie} i jestem", end =" ")
+        if self.plec=="M":
+            print("mezczyzna")
+        else:
+            print("kobieta")
+
+    def przedstaw(self, osoba):
+        print(f"Oto {osoba.imie}")
 # Gotowanie z przepisu
-adam = Czlowiek("Adam")
-ewa = Czlowiek("Ewa")
+adam = Czlowiek("Adam", "M")
+ewa = Czlowiek("Ewa", "K")
+
+class Dziecko(Czlowiek):
+    def baw_sie(self):
+        print("Ale zabawa!!!!")
+
+    def przedstaw_sie(self):
+        print(f"Czesc mam na imie {self.imie} i jestem", end =" ")
+        if self.plec=="M":
+            print("chlopcem")
+        else:
+            print("dziwczynka")
 
 print(adam.gatunek)
 print(ewa.gatunek)
 print(ewa.imie)
 print(adam.imie)
+adam.przedstaw_sie()
+ewa.przedstaw(adam)
+
+dziecko = Dziecko("Kain", "M")
+dziecko.baw_sie()
+dziecko.przedstaw_sie()

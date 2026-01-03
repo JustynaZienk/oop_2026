4c4002b781380ac9d5175890c836106a5a092740
JustynaZienk <justyna.buchnowska@gmail.com>
2026-01-03 10:27:51 +0000

    Added imie to Czlowiek konstruktor

--- klasy.py
+++ klasy.py
@@ -1,12 +1,18 @@
 #Klasa
 class Czlowiek:
     gatunek = "Homo Sapiens"
-    def __init__(self):
+    def __init__(self, imie):
         # Konstruktor
         #Akt Istnienia
-        print("Niech powstanie czowiek")
-
+        print(f"Niech powstanie czowiek o imieniu {imie}")
+        self.imie =imie
+        #ada.imie = "Adam"
 #Powstawanie obiektu
 # Gotowanie z przepisu
-adam = Czlowiek()
+adam = Czlowiek("Adam")
+ewa = Czlowiek("Ewa")
+
 print(adam.gatunek)
+print(ewa.gatunek)
+print(ewa.imie)
+print(adam.imie)

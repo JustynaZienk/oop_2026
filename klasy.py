768c32e7d083a7cc0973c319ad842d87433bce3f
JustynaZienk <justyna.buchnowska@gmail.com>
2026-01-03 10:09:08 +0000

    Initiala version the class Czlowiek

---
+++ klasy.py
@@ -0,0 +1,12 @@
+#Klasa
+class Czlowiek:
+    gatunek = "Homo Sapiens"
+    def __init__(self):
+        # Konstruktor
+        #Akt Istnienia
+        print("Niech powstanie czowiek")
+
+#Powstawanie obiektu
+# Gotowanie z przepisu
+adam = Czlowiek()
+print(adam.gatunek)
